0ae80aa12af9c74e65274c276df54555c9525871
AlexLeibch <deadlyvi@yandex.ru>
2022-03-29 17:34:31 +0200

    create filter reducer

--- src/store/filters/filter-reducer.js
+++ src/store/filters/filter-reducer.js
@@ -1,7 +1,16 @@
-import {} from "./filter-actions.js";
+import { ADD_FILTER, CLEAR_FILTER, REMOVE_FILTER } from "./filter-actions.js";
 
 export const filterReducer = (state = [], action) => {
   switch (action.type) {
+    case ADD_FILTER: {
+      return [...state, action.filter];
+    }
+    case CLEAR_FILTER: {
+      return [];
+    }
+    case REMOVE_FILTER: {
+      return state.filter((item) => item !== action.filter);
+    }
     default: {
       return state;
     }
